6322585ef43d0dc466445f03dee2c3ee3e333a69
SATYA DAS <sjcdas@gmail.com>
2018-08-31 17:01:27 +0600

    stack update

--- stack.py
+++ stack.py
@@ -8,7 +8,7 @@ class Stack:
 	def push(self,e):
 		self.data.append(e)
 
-	def pop(self,e):
+	def pop(self):
 		if self.is_empty():
 			raise Empty("stack is empty")
 		return self.data.pop()		
@@ -34,5 +34,29 @@ for i in range(1,11):
 	stack.push(i)
 	stack.print()
 for i in range(1,11):
-	stack.pop(i)
+	stack.pop()
 	stack.print()	
+
+def parChecker(symbolString):
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbolString) and balanced:
+        symbol = symbolString[index]
+        if symbol == "(":
+            s.push(symbol)
+        else:
+            if s.is_empty():
+                balanced = False
+            else:
+                s.pop()
+
+        index = index + 1
+
+    if balanced and s.is_empty():
+        return True
+    else:
+        return False
+
+print(parChecker('((()))'))
+print(parChecker('(()'))
